d77c5a4baf3a40a36d6bc1954a8994ce9674bd88
MissHarvest <abcdxsky@naver.com>
2024-06-25 21:06:01 +0900

    Vertex Data Draw Scene
    Vertex Data( vertices, normals, tangents )를 씬에서 볼 수 있도록 Gizmo를 통해서 그림.

--- Assets/2. Scripts/ProceduralMesh.cs
+++ Assets/2. Scripts/ProceduralMesh.cs
@@ -4,6 +4,7 @@ using ProceduralMeshes.Generators;
 using ProceduralMeshes.Streams;
 using UnityEngine.Rendering;
 using Unity.VisualScripting;
+using UnityEditor;
 
 [RequireComponent(typeof(MeshFilter),typeof(MeshRenderer))]
 public class ProceduralMesh : MonoBehaviour
@@ -17,6 +18,8 @@ public class ProceduralMesh : MonoBehaviour
         MeshJob<PointyHexaGrid, SingleStream>.ScheduleParallel,        
     };
 
+    public float gizmoRadius = 0.01f;
+    public float gizmoLength = 0.04f; 
     public enum MeshType
     {
         SquareGrid, 
@@ -25,6 +28,19 @@ public class ProceduralMesh : MonoBehaviour
         MySharedTriangleGrid,
         PointyHexaGrid,
     }
+
+    [System.Flags]
+    public enum GizmoMode 
+    {
+        Nothing = 0,
+        Vertices = 1,
+        Normals = 2,
+        Tangents = 4
+    }
+
+    [SerializeField]
+    GizmoMode gizmos;
+
     [SerializeField]
     MeshType meshType;
 
@@ -43,10 +59,17 @@ public class ProceduralMesh : MonoBehaviour
         GetComponent<MeshFilter>().mesh = mesh;
     }
 
+    Vector3[] vertices, normals;
+    Vector4[] tangents;
+
     private void Update()
     {
         GenerateMesh();
         enabled = false;
+
+        vertices = mesh.vertices;
+        this.normals = mesh.normals;
+        this.tangents = mesh.tangents;
     }
 
     private void OnValidate()
@@ -65,5 +88,32 @@ public class ProceduralMesh : MonoBehaviour
         jobs[(int)meshType](mesh, meshData, resolution, default).Complete();
 
         Mesh.ApplyAndDisposeWritableMeshData(meshDataArray, mesh);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (gizmos == GizmoMode.Nothing || mesh == null) return;
+
+        for(int i = 0; i < this.vertices.Length; ++i)
+        {
+            Vector3 pos = transform.TransformPoint(this.vertices[i]);
+            if ((gizmos & GizmoMode.Vertices) != 0)
+            {
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawSphere(pos, gizmoRadius);
+            }
+
+            if ((gizmos & GizmoMode.Normals) != 0)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawRay(pos, transform.TransformDirection(normals[i].normalized) * gizmoLength);
+            }
+
+            if ((gizmos & GizmoMode.Tangents) != 0)
+            {
+                Gizmos.color = Color.blue;
+                Gizmos.DrawRay(pos, transform.TransformDirection(tangents[i].normalized) * gizmoLength);
+            }
+        }
     }
 }
